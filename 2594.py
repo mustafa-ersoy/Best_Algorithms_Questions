96a3d05ce57954a95592e2c25b3f5194c4e5ed25
Mustafa Ersoy <mustafa.ersoy.tr@gmail.com>
2023-09-23 22:20:12 +0300

    fixed a solution

--- 2594.py
+++ 2594.py
@@ -1,13 +1,11 @@
 #this is the solution of Question 2594 on Leetcode
-#O(n*lgn) time, O(n) space complexity
-
+#O(n*lgk) time, O(1) space complexity where k = min(ranks)*cars**2, n = len(ranks)
 
 #when we think about it, there has to be an answer k where for any number >= k, all cars can be repaired.
 #ranks = [4,2,3], and let's say k = 18
 #first element is 4. 4 * n**2 <= 18 and n here can be 2 at most. Therefore, for a given array,
 #and k = 18, first mechanic can repair 2 cars and we can do the same of other mechanics and sum total cars.
 #that way we can convert this question into a binary search problem where we try to find the minimum possible time.
-#
 
 
 class Solution:
